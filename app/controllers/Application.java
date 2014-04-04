4029b55423815c9c24cf3696a9ffb6f9beef72d8
Jean Kahigiso M <jkahigiso@gmail.com>
2014-04-05 00:09:10 +0200

    dashboard

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,14 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
-
+import models.*;
 import views.html.*;
+
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("ZenTasks will be here"));
+        return ok(index.render(Project.find.all(),Task.find.all()));
     }
 
 }
